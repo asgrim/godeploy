498cea75fa3a5e4fda225f7a24b4d08d8f1420f8
James Titcumb <james@asgrim.com>
2011-07-25 11:54:21 +0100

    Added test data for servers for test-project

--- db/test_data.sql
+++ db/test_data.sql
@@ -53,3 +53,32 @@ VALUES (
 '2',  '1',  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDedDGG6/l6wjC44FKZ9aI6bA0S9iASLG9KbVjxvX2E9U9+dSyLalLR10PMfI4LlzNrXVlgY0Kfq4iFP31I7vo5L+8/FwTvL2aCf2rskHXDduADFVOTdXLqci5y2wmDT+rZixrpec3k7wSkJckOC/ct7iCBee3+FbZQqrnV8BdRuw== test rsa key 2',  'test rsa key 2'
 );
 
+INSERT INTO  `godeploy`.`servers` (
+`id` ,
+`name` ,
+`hostname` ,
+`connection_types_id` ,
+`port` ,
+`username` ,
+`password` ,
+`remote_path` ,
+`projects_id`
+)
+VALUES (
+1 ,  'Development server',  'dev.somesite.com',  '1',  '21',  'testusr',  'testpwd',  'public_html/',  '1'
+);
+
+INSERT INTO  `godeploy`.`servers` (
+`id` ,
+`name` ,
+`hostname` ,
+`connection_types_id` ,
+`port` ,
+`username` ,
+`password` ,
+`remote_path` ,
+`projects_id`
+)
+VALUES (
+2 ,  'Live server',  'www.somesite.com',  '1',  '21',  'testusr',  'testpwd',  'public_html/',  '1'
+);
