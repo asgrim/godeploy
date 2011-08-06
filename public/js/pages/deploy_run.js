74702913720509e2748e6684fa831de032ab0f89
James Titcumb <james@asgrim.com>
2011-08-06 15:48:08 +0100

    Issue #35 - Tried tweaking the deployment action for asynchronousness.

--- public/js/pages/deploy_run.js
+++ public/js/pages/deploy_run.js
@@ -5,7 +5,6 @@ function bootstrapDeployment()
 	var start_url = document.location.href.replace(/\/run\//, '/execute-deployment-start/');
 	
 	deployment_complete = false;
-
 	fireStatusRequest();
 	
 	new Ajax.Request(start_url, {
@@ -13,8 +12,17 @@ function bootstrapDeployment()
 			asynchronous: true,
 			onSuccess: function(transport){
 				var response = transport.responseText;
-				deployment_complete = true;
-				alert('Deplomyent finished!');
+				alert(response);
+				/*var data = response.evalJSON();
+				
+				if(data.started)
+				{
+					alert(response);
+				}
+				else
+				{
+					alert('An error happened - couldn\'t start deployment...');
+				}*/
 			},
 			onFailure: function(){ alert('Something went wrong...') }
 		});
@@ -29,15 +37,26 @@ function fireStatusRequest()
 		onSuccess: function(transport){
 			var response = transport.responseText;
 			var data = response.evalJSON();
+	
+			$('lol').innerHTML = parseInt($('lol').innerHTML) + 1;
 			
+			// Set overall status
 			$('deployment_status').innerHTML = data.OVERALL;
 			
+			// Set status of each file
 			for(var x in data.FILES)
 			{
 				$('file_' + x + '_status').innerHTML = data.FILES[x];
 			}
-			$('lol').innerHTML = parseInt($('lol').innerHTML) + 1;
 			
+			// Set that we are complete or not
+			if(data.COMPLETE)
+			{
+				alert("Deployment finished.");
+				deployment_complete = true;
+			}
+			
+			// If not complete, poll again in a moment
 			if(!deployment_complete)
 			{
 				setTimeout('fireStatusRequest()', 1000);
