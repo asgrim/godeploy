aa979a08e9cfad3648952cebfccb96149ad863b1
James Titcumb <james@asgrim.com>
2011-10-21 15:50:53 +0100

    #113 Added AUTHORS file and updated everything with standard phpdoc header

--- public/js/generate_slug.js
+++ public/js/generate_slug.js
@@ -1,4 +1,24 @@
-// JavaScript Document
+/**
+ * GoDeploy deployment application
+ * Copyright (C) 2011 the authors listed in AUTHORS file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2011 GoDeploy
+ * @author See AUTHORS file
+ * @link http://www.godeploy.com/
+ */
 
 var slug_span, slug;
 function initSlug(field)
