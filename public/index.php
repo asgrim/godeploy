b17771458785ca1f2d1fe9d8456187294011c819
James Titcumb <james@asgrim.com>
2013-05-11 08:27:40 +0100

    Updated to support composer

--- public/index.php
+++ public/index.php
@@ -8,18 +8,17 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
 	|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
-// Ensure library/ is on include_path
+// Ensure the root is on include_path
 set_include_path(
 	implode(
 		PATH_SEPARATOR, array(
-			realpath(APPLICATION_PATH . '/../library'),
+			realpath(APPLICATION_PATH . '/../'),
 			get_include_path(),
 		)
 	)
 );
 
-/** Zend_Application */
-require_once 'Zend/Application.php';
+require 'vendor/autoload.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
