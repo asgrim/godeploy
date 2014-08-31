03fda9a06407c9bd0b4186bafa863355ba20f29b
James Titcumb <james@asgrim.com>
2014-08-31 23:07:22 +0100

    Updated schema.sql with actual schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,125 @@
-CREATE TABLE `user`
-(
-    `user_id`       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username`      VARCHAR(255) DEFAULT NULL UNIQUE,
-    `email`         VARCHAR(255) DEFAULT NULL UNIQUE,
-    `display_name`  VARCHAR(50) DEFAULT NULL,
-    `password`      VARCHAR(128) NOT NULL,
-    `state`         SMALLINT UNSIGNED
-) ENGINE=InnoDB CHARSET="utf8";
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 31, 2014 at 11:06 PM
+-- Server version: 5.5.38
+-- PHP Version: 5.5.16-1+deb.sury.org~precise+1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `godeploy_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deployment`
+--
+
+CREATE TABLE IF NOT EXISTS `deployment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `revision` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `status` enum('PREVIEW','RUNNING','COMPLETE','FAILED') NOT NULL DEFAULT 'PREVIEW',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`project_id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `display_name` varchar(255) NOT NULL,
+  `git_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `target`
+--
+
+CREATE TABLE IF NOT EXISTS `target` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `hostname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `directory` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `command` varchar(512) NOT NULL,
+  `directory` varchar(255) DEFAULT NULL,
+  `only_on_targets` varchar(255) DEFAULT NULL,
+  `not_on_targets` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  `state` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `deployment`
+--
+ALTER TABLE `deployment`
+  ADD CONSTRAINT `deployment_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `deployment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `target`
+--
+ALTER TABLE `target`
+  ADD CONSTRAINT `target_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
